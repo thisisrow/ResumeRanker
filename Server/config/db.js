e8d9c98c065600b92846f3ea4eb4c27a78dd87e5
thisisrow <prathameshmishra2020@gmail.com>
2025-02-16 12:19:22 +0530

    Secure MongoDB connection by using environment variables

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
-    const url='mongodb+srv://prathameshmishra2020:%23dpmishra%3D1%2Bmongodb@cluster0.rfqwr.mongodb.net/resume?retryWrites=true&w=majority&appName=Cluster0';
   try {
-    await mongoose.connect(url);
-    console.log('MongoDB connected successfully',url);
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
